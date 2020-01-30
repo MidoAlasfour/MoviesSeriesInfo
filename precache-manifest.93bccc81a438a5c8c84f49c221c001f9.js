ad79dae6f42b397d461f531cda2e5d3528f37b0a
Mohamad Alasfour <M.alasofur@el-leasing-service.de>
2020-01-30 17:13:31 +0100

    Updates

---
+++ precache-manifest.93bccc81a438a5c8c84f49c221c001f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859a878d7c019504512f7ea04dfe069d",
+    "url": "/MoviesSeriesInfo/index.html"
+  },
+  {
+    "revision": "01429b7da81df7c0b0ae",
+    "url": "/MoviesSeriesInfo/static/css/main.01690f44.chunk.css"
+  },
+  {
+    "revision": "eaabed1c7591b23da92d",
+    "url": "/MoviesSeriesInfo/static/js/2.e2f3b5ec.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/MoviesSeriesInfo/static/js/2.e2f3b5ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01429b7da81df7c0b0ae",
+    "url": "/MoviesSeriesInfo/static/js/main.fa3b8d67.chunk.js"
+  },
+  {
+    "revision": "fb414ebbb38ce179605f",
+    "url": "/MoviesSeriesInfo/static/js/runtime-main.1b6f37a9.js"
+  }
+]);
